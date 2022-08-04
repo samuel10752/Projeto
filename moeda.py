a829d5a5158bb8e52887c7e99409aaba85a94091
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-04 12:55:33 -0300

    atulizaççao 12:55

--- moeda.py
+++ moeda.py
@@ -1,28 +1,50 @@
-from cProfile import label
-from cgitb import text
-from sqlite3 import Row
+from struct import pack
 from tkinter import *
-from tkinter import font
-from turtle import color
+import time
 
-#modelo de Janela	
+master = Tk()
+master.title("Portal Investidor")
+master.geometry("490x560+610+153")
+master.iconbitmap(default="icones\\ico.ico")
+master.resizable(width=1, height=1)
 
-root = Tk()
-altura = root.winfo_screenheight()
-largura = root.winfo_screenwidth()
-#root.overrideredirect(True)
-root.geometry('300x300+820+300') # declara o tamanho
-root.config(background='#D8D8D8') #background color
-fr1 = LabelFrame(bg='#409FDE',pady=5)
 
-#letras #(#409FDE) azul
-#front  #(#1D7334)
+# Funções
+def nova_janela():
+    master.destroy()
+    time.sleep(0.3)
 
-#widgets
-lb1_fr1 = Label(fr1, text='Tesouro Direto',foreground='#1CF00E', font='20',background='#409FDE').grid(row=0, column=1, sticky=W,padx=95)
+    master1 = Tk()
+    master1.title("Cadastro!!")
+    master1.geometry("490x560+400+153")
 
-#---Configuração do Frame---
-fr1.grid()
 
-# Executar
-root.mainloop()
+# Variáveis globais
+esconda_senha = StringVar()
+
+# Importar imagens
+img_fundo = PhotoImage(file="imagens\\fundo.png")
+img_botao = PhotoImage(file="imagens\\bt-img.png")
+img_botao2 = PhotoImage(file="imagens\\cadastro.png")
+
+# Criação de labels
+lab_fundo = Label(master, image=img_fundo)
+lab_fundo.pack()
+
+
+# Criação de caixas de entrada
+en_email = Entry(master, bd=2, font=("Calibri", 15), justify=CENTER)
+en_email.place(width=392, height=45, x=49, y=182)
+
+en_senha = Entry(master, textvariable=esconda_senha, show="*", bd=2, font=("Calibri", 15), justify=CENTER)
+en_senha.place(width=392, height=45, x=49, y=300)
+
+
+# Criação de botões
+bt_entrar = Button(master, bd=0, image=img_botao,)
+bt_entrar.place(width=118, height=64, x=280, y=408)
+
+bt_cadastro = Button(master, bd=0, image=img_botao2, command=nova_janela)
+bt_cadastro.place(width=50, height=64, x=280, y=408)
+
+master.mainloop()
