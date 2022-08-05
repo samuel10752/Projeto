cd9d3d2012e46d8a8efb05003ce190afb6092159
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-05 17:04:12 -0300

    Atualização

--- moeda.py
+++ moeda.py
@@ -5,6 +5,7 @@ from tkinter import *
 import time
 from turtle import width
 
+
 master = Tk()
 master.resizable(height = False, width = False)
 master.title("Portal Investidor")
@@ -12,24 +13,32 @@ master.geometry("480x560+800+155")
 master.iconbitmap(default="icones\\ico.ico")
 
 
+def cadastrar():
+    master.destroy()
 
+    master1 = Tk()
+    master1.title("Nova Janela criada!!")
+    master1.geometry("490x560+400+153")
+
+
+    lab2 = Label(master1, image=img_2)  
+    lab2.grid()
 # Variáveis globais
 esconda_senha = StringVar()
 
 
 # Importar imagens
-img1 = PhotoImage(file="imagens\\fundo.png")
-img2 = PhotoImage(file="imagens\\fundo2.png")
-img3 = PhotoImage(file="imagens\\bt-img.png")
-img4 = PhotoImage(file="imagens\\cadastro.png")
-img5 = PhotoImage(file="imagens\\cadastrar.png")
+img_1 = PhotoImage(file="imagens\\fundo.png")
+img_2 = PhotoImage(file="imagens\\fundo2.png")
+img_3 = PhotoImage(file="imagens\\bt-img.png")
+img_4 = PhotoImage(file="imagens\\cadastro.png")
+img_5 = PhotoImage(file="imagens\\cadastrar.png")
 
 # Criação de labels
-lab1 = Label(master, image=img1)
-lab1.place(x=0, y=0)
+lab = Label(master, image=img_1)
+lab.pack()
 
-#lab2 = Label(master, image=img2)
-#lab2.place(x=0, y=0)
+
 
 
 # Criação de caixas de entrada
@@ -41,10 +50,10 @@ senha1.place(width=392, height=45, x=43, y=300)
 
 
 # Criação de botões
-entrar1 = Button(master, bd=0, image=img1)
-entrar1.place(width=120, height=64, x=270, y=408)
+entrar1 = Button(master, bd=0, image=img_3)
+entrar1.place(width=118, height=64, x=270, y=408)
 
-cadastro1 = Button(master, bd=0, image=img2, command= lambda: [fr0.place_forget(), fr1.place(x=0, y=0)])
+cadastro1 = Button(master, bd=0, image=img_4, command= cadastrar)
 cadastro1.place(width=175, height=64, x=68, y=408)
 
 master.mainloop()
