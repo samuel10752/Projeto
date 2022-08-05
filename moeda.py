ce4ea56cf396216d002a5af0b6d81df713cf4da8
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-05 11:03:03 -0300

    Atualização 11:00

--- moeda.py
+++ moeda.py
@@ -1,63 +1,66 @@
+from math import fabs
 from struct import pack
 from tkinter import *
 import time
+from turtle import width
 
 master = Tk()
+master.resizable(height = False, width = False)
 master.title("Portal Investidor")
-master.geometry("490x560+800+153")
+master.geometry("480x560+800+155")
 master.iconbitmap(default="icones\\ico.ico")
-master.resizable(width=1, height=1)
+
 
 
 # Funções
-def Cadastro():
-    master.destroy()
-    time.sleep(0.3)
+#def Cadastro():
+#    master.destroy()
+#    time.sleep(0.3)
+#
+#    fr1.title("Cadastro !")
+#    fr1.geometry("500x560+800+153") 
+#
+#
+#def Tela_investimento():
+#    master.destroy()
+#    time.sleep(0.3)
+#
+#    fr2 = Tk()
+#    fr2.title("tela de investimento !")
+#    fr2.geometry("1200x500+400+153")  
 
-    master1 = Tk()
-    master1.title("Cadastro !")
-    master1.geometry("490x560+800+153")
-
-
-def Tela_investimento():
-    master.destroy()
-    time.sleep(0.3)
-
-    master1 = Tk()
-    master1.title("tela de investimento !")
-    master1.geometry("1200x500+400+153")    
+   
 
 
 # Variáveis globais
 esconda_senha = StringVar()
 
 # Importar imagens
-img_fundo = PhotoImage(file="imagens\\fundo.png")
-img_fundo2 = PhotoImage(file="imagens\\fundo2.png")
-img_botao = PhotoImage(file="imagens\\bt-img.png")
-img_botao2 = PhotoImage(file="imagens\\cadastro.png")
+img_fundo_fr0 = PhotoImage(file="imagens\\fundo.png")
+img_fundo_fr1 = PhotoImage(file="imagens\\fundo2.png")
+img_botao_fr0 = PhotoImage(file="imagens\\bt-img.png")
+img_botao2_fr0 = PhotoImage(file="imagens\\cadastro.png")
+img_botao3_fr1 = PhotoImage(file="imagens\\cadastrar.png")
 
 # Criação de labels
-lab_fundo = Label(master, image=img_fundo)
-lab_fundo.pack()
+lab_fundo_fr0 = Label(master, image=img_fundo_fr0).pack()
 
-lab_fundo2 = Label(master, image=img_fundo2)
-lab_fundo2.pack()
+lab_fundo_fr1 = Label(master, image=img_fundo_fr1)
 
 
 # Criação de caixas de entrada
-en_email = Entry(master, bd=2, font=("Calibri", 15), justify=CENTER)
-en_email.place(width=392, height=45, x=49, y=182)
+en_email_fr0 = Entry(master, bd=2, font=("Calibri", 15), justify=CENTER)
+en_email_fr0.place(width=392, height=45, x=43, y=182)
 
-en_senha = Entry(master, textvariable=esconda_senha, show="*", bd=2, font=("Calibri", 15), justify=CENTER)
-en_senha.place(width=392, height=45, x=49, y=300)
+en_senha_fro = Entry(master, textvariable=esconda_senha, show="*", bd=2, font=("Calibri", 15), justify=CENTER)
+en_senha_fro.place(width=392, height=45, x=43, y=300)
 
 
 # Criação de botões
-bt_entrar = Button(master, bd=0, image=img_botao,command=Tela_investimento)
-bt_entrar.place(width=118, height=64, x=280, y=408)
+bt_entrar_fr0 = Button(master, bd=0, image=img_botao_fr0)
+bt_entrar_fr0.place(width=120, height=64, x=270, y=408)
 
-bt_cadastro = Button(master, bd=0, image=img_botao2, command=Cadastro)
-bt_cadastro.place(width=171, height=64, x=74, y=408)
+bt_cadastro_fr0 = Button(master, bd=0, image=img_botao2_fr0)
+bt_cadastro_fr0.place(width=175, height=64, x=68, y=408)
 
 master.mainloop()
