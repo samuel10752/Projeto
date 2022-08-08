442b15d220d2926f9b310516cc8117043487ad6c
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-08 10:14:53 -0300

    Atualização

--- investidor.py
+++ investidor.py
@@ -20,7 +20,9 @@ fr2_img_4 = PhotoImage(file="imagens\\vender.png")
 fr2_img_5 = PhotoImage(file="imagens\\tabela.png")
 
 #frame 3
-fr2_img_6= PhotoImage(file="imagens\\compra.png")
+#fr2_img_6= PhotoImage(file="imagens\\compra.png")
+#fr2_img_6= PhotoImage(file="imagens\\venda.png")
+#fr2_img_6= PhotoImage(file="imagens\\moeda e investir.png")
 
 # Criação de labels 
 
@@ -36,9 +38,32 @@ fr2_bt3 = Button(fr2, bd=0, image=fr2_img_4).place(width=223, height=60, x=367, 
 
 # frame 3
 # Criação de labels 
+#fr2_lab2 = Label(fr2,bd=0, image=fr2_img_6).place(width=310, height=470, x=970, y=130)
 
-fr2_lab2 = Label(fr2,bd=0, image=fr2_img_6).place(width=310, height=470, x=970, y=130)
 
+fr3 = Frame()
+
+
+#frame 3
+fr3_img_1 = PhotoImage(file="imagens\\compra.png")
+#fr2_img_6= PhotoImage(file="imagens\\venda.png")
+#fr2_img_6= PhotoImage(file="imagens\\moeda e investir.png")
+
+# Criação de labels 
+
+fr2_lab = Label(fr2, image=fr2_img_1, width=1285).grid(row=0,column=0,sticky=W)
+fr2_lab1 = Label(fr2,bd=0, image=fr2_img_5).place(width=305, height=65, x=974, y=26)
+
+# Criação de botões 
+
+fr2_bt1 = Button(fr2, bd=0, image=fr2_img_2,command=lambda: [fr2.grid_remove(),fr3.place()]).place(width=223, height=60, x=675, y=32)
+fr2_bt2 = Button(fr2, bd=0, image=fr2_img_3).place(width=223, height=60, x=54, y=32)
+fr2_bt3 = Button(fr2, bd=0, image=fr2_img_4).place(width=223, height=60, x=367, y=32)
+
+
+# frame 3
+# Criação de labels 
+fr3_lab = Label(fr3,bd=0, image=fr3_img_1).place(width=310, height=470, x=970, y=130)
 
 fr2.grid()
 root.mainloop()
