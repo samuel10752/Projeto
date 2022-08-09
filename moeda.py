c847b38b210cbdfc1af1e754063a130ae70a8599
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-09 11:18:50 -0300

    atualização

--- moeda.py
+++ moeda.py
@@ -11,7 +11,7 @@ from turtle import title, width
 
 root = Tk()
 root.title("Portal Investidor")
-root.resizable(height = False, width = False)
+#root.resizable(height = False, width = False)
 root.geometry("484x560+800+155")
 root.iconbitmap(default="icones\\ico.ico")
 
@@ -141,7 +141,7 @@ fr3_img_3 = PhotoImage(file="imagens\\comprar.png")
 fr3_img_4 = PhotoImage(file="imagens\\vender.png")
 fr3_img_5 = PhotoImage(file="imagens\\tabela.png")
 fr3_img_6 = PhotoImage(file="imagens\\compra.png")
-
+fr3_img_7 = PhotoImage(file="imagens\\comprar.png")
 #frame 4
 fr4_img_1 = PhotoImage(file="imagens\\tela.png")
 fr4_img_2 = PhotoImage(file="imagens\\guardar.png")
@@ -256,11 +256,12 @@ fr2_bt3 = Button(fr2, bd=0, image=fr2_img_4, command= lambda:[fr2.grid_remove(),
 
 # Criação de labels 
 
+
 # tela do Investidor de Compra
 fr3_lab = Label(fr3, image=fr3_img_1, width=1285).grid(row=0,column=0,sticky=W)
 
 # imahem compra
-fr3_lab1 = Label(fr3,bd=0, image=fr3_img_6).place(width=310, height=470, x=970, y=130)
+fr3_lab1 = Label(fr3, image=fr3_img_6,bd=0).place(width=310, height=470, x=970, y=130)
 
 # image tabela
 fr3_lab2 = Label(fr3,bd=0, image=fr3_img_5).place(width=305, height=65, x=974, y=26)
@@ -275,6 +276,7 @@ fr3_bt2 = Button(fr3, bd=0, image=fr3_img_3, command= lambda:[fr3.grid_remove(),
 fr3_bt3 = Button(fr3, bd=0, image=fr3_img_4, command= lambda:[fr3.grid_remove(),fr4.grid(),root.geometry("1289x600+310+153")]).place(width=223, height=60, x=367, y=32)
 
 
+fr5_in1 = Entry(fr5, bd=5, font=("Calibri", 15)).place(width=392, height=45, x=22, y=343)
 # frame 4 # tela da Venda
 
 # Criação de labels 
@@ -296,6 +298,9 @@ fr4_bt1 = Button(fr4, bd=0, image=fr4_img_2, command= lambda:[fr4.grid_remove(),
 fr4_bt2 = Button(fr4, bd=0, image=fr4_img_3, command= lambda:[fr4.grid_remove(),fr3.grid(),root.geometry("1289x600+310+153")]).place(width=223, height=60, x=54, y=32)
 # Botão de vender
 fr4_bt3 = Button(fr4, bd=0, image=fr4_img_4, command= lambda:[fr4.grid_remove(),fr4.grid(),root.geometry("1289x600+310+153")]).place(width=223, height=60, x=367, y=32)
+
+
+fr4_in1 = Entry(fr4, bd=5, font=("Calibri", 15)).place(width=392, height=45, x=22, y=343)
 
 # frame 5 Tela de Guardar
 
@@ -319,6 +324,7 @@ fr5_bt2 = Button(fr5, bd=0, image=fr5_img_3,command= lambda:[fr5.grid_remove(),f
 # Botão de venda
 fr5_bt3 = Button(fr5, bd=0, image=fr5_img_4,command= lambda:[fr5.grid_remove(),fr4.grid(),root.geometry("1289x600+310+153")]).place(width=223, height=60, x=367, y=32)
 
+fr5_in1 = Entry(fr5, bd=5, font=("Calibri", 15)).place(width=392, height=45, x=22, y=343)
 
 fr0.grid()
 root.mainloop()
